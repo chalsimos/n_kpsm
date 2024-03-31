e30e23875c49098668a7a723ca1c933e9fd524a6
chalsimos <krizchan31@gmail.com>
2024-03-31 22:15:35 +0800

    frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import axios  from 'axios'
-axios.defaults.baseURL="http://kpsm.test/api/"
+
 createApp(App).use(router).mount('#app')
