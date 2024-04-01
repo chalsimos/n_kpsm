bbb09e2bb33b2eb85aa027fe3afc0b2f1f72a2be
chalsimos <krizchan31@gmail.com>
2024-04-01 21:17:32 +0800

    update requestcontroller
    modify basecontroller to resourcecontroller
    update the constructor
    extract data

--- backend/app/Controllers/RequestController.php
+++ backend/app/Controllers/RequestController.php
@@ -3,10 +3,26 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\RequestModel;
 
-class RequestController extends BaseController
+class RequestController extends ResourceController
 {
+    protected $format       = 'json';
+
+    public function __construct(){
+        $this->requestModel = new RequestModel;
+    }
+    public function getRequest()
+    {
+        // generate and return all types of request from all users
+        // applicable to execute all data and return to superadmin and admin
+        //generate 4.1.2024 8.06P
+        $data = $this->requestModel->findAll();
+        var_dump($data);
+    }
     public function index()
     {
         //
