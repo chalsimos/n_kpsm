7f718002dc202dc1ccf7eb1018bf9a495a1efdc8
Carpel Sweet Dreams B. Lanto <carpelbalmes@gmail.com>
2024-04-02 00:27:15 +0800

    add tailwind, flowbite, test design

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,25 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store';
+import './assets/index.css'
+import 'flowbite'
 
 import axios from 'axios'
 axios.defaults.baseURL="http://n_kpsm.test/api/";
 export default axios;
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 createApp(App)
 .use(router)
 .use(store)
+.use(vuetify)
 .mount('#app')
