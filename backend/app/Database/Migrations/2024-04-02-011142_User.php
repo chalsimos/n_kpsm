fbb7fe0e135e5bddf479fb432a017d20e55d30de
chalsimos <krizchan31@gmail.com>
2024-04-02 09:26:59 +0800

    added new migration

---
+++ backend/app/Database/Migrations/2024-04-02-011142_User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' =>true,
+                'constraint' => 5,
+                'auto_increment' =>true,
+            ],
+            'email' =>[
+                'type' =>'TEXT',
+            ],
+            'password' =>[
+                'type' =>'TEXT',
+            ],
+            'token' =>[
+                'type' =>'TEXT',
+            ],
+            'remember_token' =>[
+                'type' =>'TEXT',
+                'null' =>true
+            ],
+            'status' =>[
+                'type' =>'TEXT',
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
