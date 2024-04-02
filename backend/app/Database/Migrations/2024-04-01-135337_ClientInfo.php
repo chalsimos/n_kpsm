7f50abf4a890e523cc9d633568c8bb0392d43dfb
chalsimos <krizchan31@gmail.com>
2024-04-02 09:10:32 +0800

    change routes

---
+++ backend/app/Database/Migrations/2024-04-01-135337_ClientInfo.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class ClientInfo extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' =>true,
+                'auto_increment' => true
+            ],
+            'FirstName' =>[
+                'type' => 'TEXT',
+            ],
+            'LastName' => [
+                'type' => 'TEXT'
+            ],
+            'phone' => [
+                'type' => 'TEXT'
+            ],
+            'email' => [
+                'type' => 'TEXT'
+            ],
+            'municipality' => [
+                'type' => 'TEXT'
+            ],
+            'barangay' => [
+                'type' =>'TEXT'
+            ],
+            'sitio' => [
+                'type' => 'TEXT',
+                'null' => true
+            ]
+            ]);
+            $this->forge->addPrimaryKey('id');
+            $this->forge->createTable('clientinfo');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('clientinfo');
+    }
+}
