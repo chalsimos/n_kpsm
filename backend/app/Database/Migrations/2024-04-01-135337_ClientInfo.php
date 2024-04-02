aa9e265f7c4586cd9e0ce47ffdf42c997e2add5a
chalsimos <krizchan31@gmail.com>
2024-04-02 13:35:10 +0800

    update

--- backend/app/Database/Migrations/2024-04-01-135337_ClientInfo.php
+++ backend/app/Database/Migrations/2024-04-01-135337_ClientInfo.php
@@ -9,7 +9,7 @@ class ClientInfo extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id' => [
+            'id' => [   
                 'type' => 'INT',
                 'constraint' => 5,
                 'unsigned' =>true,
