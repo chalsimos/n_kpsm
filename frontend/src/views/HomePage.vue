2fa149bb1a2de43ac9383c4e4d52d7c807f5a85c
chalsimos <krizchan31@gmail.com>
2024-04-07 12:35:11 +0800

    added homepage
    added homepage to interact with user/ news and announcement portal also

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,10 +1,17 @@
 <!-- carpel, ito ang landing page, dito narin ung mga news -->
 <template>
-eto na
+<hd/>
 </template>
 
 <script>
+import hd from  '../views/main/hd.vue';
 export default{
-
+    components:{
+        hd
+    },
+    mounted(){
+        document.title='KPSM - kongreso para sa mamamayan';
+    },
+    
 }
 </script>
