d832d63f3e167920b7bce836a12029f78498d523
Carpel Sweet Dreams B. Lanto <carpelbalmes@gmail.com>
2024-04-07 20:56:32 +0800

    modified userview added footer, make header fixed, add body div in homepage

--- frontend/src/views/UserView/Home/HomePage.vue
+++ frontend/src/views/UserView/Home/HomePage.vue
@@ -1,16 +1,17 @@
 <!-- carpel, ito ang landing page, dito narin ung mga news -->
 <template>
-<Head/>
+  <Head />
 </template>
 
 <script>
-import Head from  '@/views/UserView/Home/Header.vue';
+import Head from "@/views/UserView/Home/Header.vue";
 
 export default {
-	components: {
-		Head
-	},
-	mounted() {
-		document.title = "KPSM - kongreso para sa mamamayan";
-			},};
+  components: {
+    Head,
+  },
+  mounted() {
+    document.title = "KPSM - kongreso para sa mamamayan";
+  },
+};
 </script>
