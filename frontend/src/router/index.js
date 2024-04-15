455c2df6895e2dd4e6ded0dbc834eaab9e1a3481
Carpel Sweet Dreams B. Lanto <carpelbalmes@gmail.com>
2024-04-15 12:41:44 +0800

    changes in design, installed vue-datatable,

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,14 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import store from '@/store';
 
 const routes = [
-	{ path:'/', name:'Home Page', component:() => import('../views/UserView/Home/HomePage.vue')},
-	{ path:'/medical-request', name:'Medical Request', component:() => import('../views/UserView/Medical/MedicalRequest.vue'),meta: { requiresAuth: true }},
-	{ path:'/scholarship', name:'scholarship', component:() => import('../views/UserView/Scholarship/ScholarRequest.vue'),meta: { requiresAuth: true }},
-	{ path: '/admin', name: 'Home', component: () => import('../views/AdminViews/Home/HomeView.vue') },
+	//Account
 	{ path: '/login', name: 'Login', component: () => import('../views/Account/Login.vue') }, 
 	{ path: '/register', name: 'Register', component: () => import('../views/Account/Register.vue') }, 
 	{ path: '/forgot-password', name: 'Forgot Password', component: () => import('../views/Account/ForgotPassword.vue') }, 
-	{ path: '/change-password', name: 'Change Password', component: () => import('../views/Account/ChangePassword.vue') }
+	{ path: '/change-password', name: 'Change Password', component: () => import('../views/Account/ChangePassword.vue') },
+
+	//User Side
+	{ path:'/', name:'Home Page', component:() => import('../views/UserView/Home/HomePage.vue')},
+	{ path:'/medical-request', name:'Medical Request', component:() => import('../views/UserView/Medical/MedicalRequest.vue'),meta: { requiresAuth: true }},
+	{ path:'/scholarship', name:'scholarship', component:() => import('../views/UserView/Scholarship/ScholarRequest.vue'),meta: { requiresAuth: true }},
+
+	//Admin
+	{ path: '/admin', name: 'Home', component: () => import('../views/AdminViews/Home/HomeView.vue') },
+	{ path: '/manage-medical-request', name: 'Manage Medical Request', component: () => import('../views/AdminViews/Medical/ManageMedicalRequest.vue') },
+
 ]
 
 const router = createRouter({
--- frontend/src/views/AdminViews/Medical/ManageMedicalRequest.vue
+++ frontend/src/views/AdminViews/Medical/ManageMedicalRequest.vue
@@ -1,21 +1,24 @@
 <template>
-
 <Side />
 
 <Foot />
 </template>
 
+    
 <script>
 // @ is an alias to /src
 import Side from '@/views/AdminViews/Home/Sidebar.vue'
 import Foot from "@/views/AdminViews/Home/Footer.vue";
-
+import DataTable from 'datatables.net-vue3';
+import DataTablesCore from 'datatables.net';
+DataTable.use(DataTablesCore);
 export default {
-	components: {
-		Side,
+    components: {
+        Side,
         Foot,
-	},
-	mounted() {
-		document.title = "Home";
-			},};
+    },
+    mounted() {
+        document.title = "Home";
+    },
+};
 </script>
