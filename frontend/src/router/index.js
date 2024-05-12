89475fb2a117aa0d2a7e56133c2f35da75651ae4
chalsimos <krizchan31@gmail.com>
2024-05-12 18:42:18 +0800

    hello

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,9 @@ const toastr = useToast()
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
+        //need invitation code from captain
+        // { path:'/tupad', name:'Tupad', component:() => import('../views/UserView/Dole/Tupad.vue')},
+
         //Account
         { path: '/login', name: 'Login', component: () => import('../views/Account/Login.vue') },
         { path: '/register', name: 'Register', component: () => import('../views/Account/Register.vue') },
@@ -16,20 +19,16 @@ const router = createRouter({
         { path: '/change-password', name: 'Change Password', component: () => import('../views/Account/ChangePassword.vue') },
 
         //User Side
-        { path:'/', name:'Home Page', component:() => import('../views/UserView/Home/HomePage.vue')},
+        { path:'/', name:'HomePage', component:() => import('../views/UserView/Home/HomePage.vue')},
         { path:'/medical-request', name:'Medical Request', component:() => import('../views/UserView/Medical/MedicalRequest.vue'),meta: { requiresClient: true}},
         { path:'/scholarship', name:'scholarship', component:() => import('../views/UserView/Scholarship/ScholarRequest.vue'),meta: { requiresClient: true}},
 
-        //Brgy. Captain Side
-        { path:'/tupad', name:'Tupad', component:() => import('../views/UserView/Dole/Tupad.vue'),meta: {  requiresCaptain: true}},
+        // Brgy. Captain Side
         { path:'/gip', name:'GIP', component:() => import('../views/UserView/Dole/GIP.vue'),meta: {  requiresCaptain: true }},
-<<<<<<< Updated upstream
-    
-=======
-        { path:'/tupad-slot', name:'GIP', component:() => import('../views/UserView/Dole/TupadSlot.vue'),meta: {  requiresCaptain: true }},
+        // { path:'/tupad-slot', name:'GIP', component:() => import('../views/UserView/Dole/TupadSlot.vue'),meta: {  requiresCaptain: true }},
 
->>>>>>> Stashed changes
         //Admin
+        // { path: '/manage-tupad', name: 'Manage Tupad', component: () => import('../views/AdminViews/Dole/ManageTupad.vue') ,meta: {  requiresAdmin: true }},
         { path: '/admin', name: 'Home', component: () => import('../views/AdminViews/Home/HomeView.vue') ,meta: {  requiresAdmin: true }},
         { path: '/manage-medical-request', name: 'Manage Medical Request', component: () => import('../views/AdminViews/Medical/ManageMedicalRequest.vue') ,meta: {  requiresAdmin: true }},
         // { path: '/admin/all-news', name: 'Home', component: () => import('../views/AdminViews/News/NewsManagement.vue') ,meta: {  requiresAdmin: true }},
--- frontend/src/views/UserView/Home/HomePage.vue
+++ frontend/src/views/UserView/Home/HomePage.vue
@@ -1,107 +1,323 @@
+
 <template>
-<<<<<<< Updated upstream
-  <Head />
-<div class="p-4 mt-[5vh]">
-    <div
-      class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14"
-    >
-=======
   <div class="flex flex-col min-h-screen">
-    <Head />
-    <div class="flex-grow p-4 mt-[5vh]">
->>>>>>> Stashed changes
-      <div class="grid grid-cols-3 gap-4 mb-4">
-        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-          <div v-for="(headline, index) in headlines" :key="index" class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p class="text-lg text-gray-700 dark:text-gray-300">{{ headline.title }}</p>
-
-          </div>
-        </div>
-        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">Featured News</p>
-        </div>
-        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-          <p class="text-2xl text-gray-400 dark:text-gray-500">More News</p>
-        </div>
-      </div>
-      <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-        <p class="text-2xl text-gray-400 dark:text-gray-500">Featured Article</p>
-      </div>
-      <div class="grid grid-cols-2 gap-4 mb-4">
-        <div v-for="(article, index) in articles" :key="index" class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-          <div>
-            <p class="text-lg text-gray-700 dark:text-gray-300">{{ article.title }}</p>
-            <p class="text-sm text-gray-500 dark:text-gray-400">By {{ article.author }} | {{ article.date }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <Foot />
-  </div>
-<<<<<<< Updated upstream
-  <div class="inset-x-0 bottom-0 py-4">
-        <Foot />
-    </div>
-=======
->>>>>>> Stashed changes
-</template>
-
-
-<script>
-import {
-    Modal,
-    Tooltip,
-    initTWE
-} from "tw-elements";
-import {
-    initFlowbite
-} from 'flowbite'
-import Head from "@/views/UserView/Home/Header.vue";
-import Foot from "@/views/UserView/Home/Footer.vue";
-import axios from 'axios';
-export default {
-  components: {
-    Head,
-    Foot
-  },
-  data() {
-    return {
-      articles: [],
-      headlines: []
-    };
-  },
-  mounted() {
-    initTWE({
-        Modal,
-        Tooltip
-    });
-    initFlowbite();
-    document.title = "KPSM - Kongreso para sa Mamamayan";
-    this.getNews();
-    this.getHeadLine();
-  },
+         <Head />
+         <div class="flex-grow p-4 mt-[5vh]">
+     <div
+       class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14"
+     >
+       <div class="grid grid-cols-3 gap-4 mb-4">
+         <div
+           class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+         <div
+           class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+         <div
+           class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+       </div>
+       <div
+         class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800"
+       >
+         <p class="text-2xl text-gray-400 dark:text-gray-500">
+           <svg
+             class="w-3.5 h-3.5"
+             aria-hidden="true"
+             xmlns="http://www.w3.org/2000/svg"
+             fill="none"
+             viewBox="0 0 18 18"
+           >
+             <path
+               stroke="currentColor"
+               stroke-linecap="round"
+               stroke-linejoin="round"
+               stroke-width="2"
+               d="M9 1v16M1 9h16"
+             />
+           </svg>
+         </p>
+       </div>
+       <div class="grid grid-cols-2 gap-4 mb-4">
+         <div
+           class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+         <div
+           class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+         <div
+           class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+         <div
+           class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+       </div>
+       <div
+         class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800"
+       >
+         <p class="text-2xl text-gray-400 dark:text-gray-500">
+           <svg
+             class="w-3.5 h-3.5"
+             aria-hidden="true"
+             xmlns="http://www.w3.org/2000/svg"
+             fill="none"
+             viewBox="0 0 18 18"
+           >
+             <path
+               stroke="currentColor"
+               stroke-linecap="round"
+               stroke-linejoin="round"
+               stroke-width="2"
+               d="M9 1v16M1 9h16"
+             />
+           </svg>
+         </p>
+       </div>
+       <div class="grid grid-cols-2 gap-4">
+         <div
+           class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+         <div
+           class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+         <div
+           class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+         <div
+           class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+         >
+           <p class="text-2xl text-gray-400 dark:text-gray-500">
+             <svg
+               class="w-3.5 h-3.5"
+               aria-hidden="true"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 18 18"
+             >
+               <path
+                 stroke="currentColor"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 stroke-width="2"
+                 d="M9 1v16M1 9h16"
+               />
+             </svg>
+           </p>
+         </div>
+       </div>
+     </div>
+   </div>
+   <div>
+             <Foot />
+         </div>
+     </div>
+ </template>
  
- 
-  methods:{
-    async getHeadLine(){
-      try {
-        const response = await axios.get("/api/headline");
-        this.headlines = response.data;
-      }catch (error){
-        console.log("error retrieving headline", error);
-      }
-    },
-    async getNews() {
-    try {
-      const response = await axios.get("/api/news");
-      const ne = response.data;
-      // console.log("Fetched news data:", ne); // Log fetched data
-      this.articles = ne.data; 
-    } catch (error) {
-      console.error("Error fetching news:", error);
-    }
-  },
-  }
-};
-</script>
-
+ <script>
+ import {
+ } from "ant-design-vue";
+ import {
+     Modal,
+     Tooltip,
+     initTWE
+ } from "tw-elements";
+ import {
+     initFlowbite
+ } from 'flowbite'
+ import Head from "@/views/UserView/Home/Header.vue";
+ import Foot from "@/views/UserView/Home/Footer.vue";
+ export default {
+   components: {
+     Head,
+     Foot
+   },
+   mounted() {
+     initTWE({
+         Modal,
+         Tooltip
+     });
+     initFlowbite();
+     document.title = "KPSM - Kongreso para sa Mamamayan";
+   },
+ };
+ </script>
